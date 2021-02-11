1c92a1f1fe6e6b66a8f1336d23f3ce16a2576dba
Svitlana Rybakova <rsv0011@gmail.com>
2021-02-11 14:26:39 +0100

    add more context

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
 import ThemeContextProvaider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext'
 
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvaider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle/>
+          <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle/>
+        </AuthContextProvider>
       </ThemeContextProvaider>
     </div>
   );
