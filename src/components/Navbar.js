46abd0b8742a34ce0a1f765ad114341ab916ff86
Svitlana Rybakova <rsv0011@gmail.com>
2021-02-11 12:11:41 +0100

    create layout and styles

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render(){
+    return( 
+      <nav>
+      <h1>Content App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contacts</li>
+      </ul>
+      </nav> 
+    )
+      
+    
+  }
+}
+export default Navbar;
