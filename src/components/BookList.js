c3b878f8c878eebbd88b00caf15f9b90c90716b0
Svitlana Rybakova <rsv0011@gmail.com>
2021-02-11 13:27:47 +0100

    another way ti transfer Context via Consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext'
 
 class BookList extends Component {
+  // так как использую класс, другой метод передачи контекста
+  // contextType обязательное навзвание, ему присваиваю переменную в которой есть контекст
+
   static contextType = ThemeContext;
 
   render() {
 
+    // показывает доступ к контексту через contextType
+    // console.log(this.context)
+    // dustructuring
     const { isLightTheme, light, dark } = this.context;
     const theme = isLightTheme ? light : dark;
 
     return (
-      <div className="book-list" style={{ background: theme.bg,  color: theme.syntax}}>
+      <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
         <ul>
           <li style={{ background: theme.ui }}>the name of the wind</li>
           <li style={{ background: theme.ui }}>the final empire</li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  // так как использую класс, другой метод передачи контекста
-  // contextType обязательное навзвание, ему присваиваю переменную в которой есть контекст
-  static contextType = ThemeContext;
+  
 
-  render(){
-    // показывает доступ к контексту через contextType
-    console.log(this.context);
-    // destructuring
-    const { isLightTheme, light, dark } = this.context;
-    console.log(isLightTheme, light, dark);
-    
-    const theme = isLightTheme ? light : dark;
-    return( 
-      <nav style={{ background: theme.ui,  color: theme.syntax}}>
-      <h1>Content App</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contacts</li>
-      </ul>
-      </nav> 
+  render() {
+;
+
+
+    return (
+      // другой способ передать контекст через Comsumer
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Content App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contacts</li>
+            </ul>
+          </nav>
+        )
+      }}
+
+      </ThemeContext.Consumer>
     )
-      
-    
+
+
   }
 }
 export default Navbar;
