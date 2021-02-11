ef7801a8e7e3030f8ea4c0f3a0e4d2723891c8d6
Svitlana Rybakova <rsv0011@gmail.com>
2021-02-11 20:10:36 +0100

    Creating Context with Functional Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
 import ThemeContextProvaider from './contexts/ThemeContext';
 import AuthContextProvider from './contexts/AuthContext'
+import BookContextProvider from './contexts/BookContext';
 
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
       <ThemeContextProvaider>
           <AuthContextProvider>
           <Navbar />
-          <BookList />
+            <BookContextProvider>
+              <BookList />
+            </BookContextProvider>
           <ThemeToggle/>
         </AuthContextProvider>
       </ThemeContextProvaider>
